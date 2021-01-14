eefd218ea5b0d1008bb3efc4e53e0b20cc2e8d57
Jithendra <jithendrads@gmail.com>
2021-01-14 20:03:52 +0530

    created optional request parameter

---
+++ mobile-app-ws/src/main/java/com/jithendra/mobileappws/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.jithendra.mobileappws.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	@GetMapping()
+	public String getUser(@RequestParam(value="page", defaultValue="1") int page,@RequestParam(value="limit", defaultValue="50") int limit )
+	{
+		return "get user was called with page = "+page+" and limit "+limit;
+	}
+	
+	@GetMapping("/{userId}")
+	public String getUser(@PathVariable int userId)
+	{
+		return "get user was called with userId "+userId;
+	}
+	@PostMapping
+	public String createUser()
+	{
+		return "create user was called";
+	}
+	
+	@PutMapping
+	public String updateUser()
+	{
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser()
+	{
+		return "delete user was called";
+	}
+
+}
