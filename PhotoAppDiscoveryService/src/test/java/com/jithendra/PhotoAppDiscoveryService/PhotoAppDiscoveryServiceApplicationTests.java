1ca981f903255cf1bb9b67d04563a2c9ad37efa8
Jithendra <jithendrads@gmail.com>
2021-01-15 18:12:22 +0530

    created Eureka discovery service

---
+++ PhotoAppDiscoveryService/src/test/java/com/jithendra/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jithendra.PhotoAppDiscoveryService;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
