56bd4c3d61eb95895fcab4fdb3bb1031321b048c
Jithendra <jithendrads@gmail.com>
2021-01-15 13:31:16 +0530

    sample Restful api

--- mobile-app-ws/src/main/java/com/jithendra/mobileappws/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/jithendra/mobileappws/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.jithendra.mobileappws.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jithendra.mobileappws.model.User;
+import com.jithendra.mobileappws.model.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -18,15 +28,32 @@ public class UserController {
 		return "get user was called with page = "+page+" and limit "+limit;
 	}
 	
-	@GetMapping("/{userId}")
-	public String getUser(@PathVariable int userId)
+	@GetMapping(path="/{userId}", produces= { MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<User> getUser(@PathVariable int userId)
 	{
-		return "get user was called with userId "+userId;
+		User user=new User();
+		user.setEmail("abc@gmail.com");
+		user.setFirstName("jithu");
+		user.setLastName("endra");
+		user.setUserId("1");
+		return new ResponseEntity<User>(user,HttpStatus.OK);
 	}
-	@PostMapping
-	public String createUser()
+	@PostMapping(
+		consumes= 
+			{ MediaType.APPLICATION_XML_VALUE,
+			  MediaType.APPLICATION_JSON_VALUE
+			},
+		produces= 
+			{ MediaType.APPLICATION_XML_VALUE,
+			  MediaType.APPLICATION_JSON_VALUE
+			})
+	public ResponseEntity<User> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails)
 	{
-		return "create user was called";
+		User user=new User();
+		user.setEmail(userDetails.getEmail());
+		user.setFirstName(userDetails.getFirstName());
+		user.setLastName(userDetails.getLastName());
+		return new ResponseEntity<User>(user,HttpStatus.OK);
 	}
 	
 	@PutMapping
