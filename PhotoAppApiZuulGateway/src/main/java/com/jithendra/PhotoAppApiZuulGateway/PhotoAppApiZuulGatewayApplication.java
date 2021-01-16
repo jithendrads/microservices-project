d3ad5bcc961b3a3b3d5c579a9ba7d132dc6b6b97
Jithendra <jithendrads@gmail.com>
2021-01-16 19:11:39 +0530

    created spring cloud api gateway

---
+++ PhotoAppApiZuulGateway/src/main/java/com/jithendra/PhotoAppApiZuulGateway/PhotoAppApiZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.jithendra.PhotoAppApiZuulGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class PhotoAppApiZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiZuulGatewayApplication.class, args);
+	}
+
+}
