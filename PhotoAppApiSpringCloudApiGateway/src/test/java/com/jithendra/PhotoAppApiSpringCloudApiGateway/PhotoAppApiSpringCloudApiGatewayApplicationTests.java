1300f19926ec99ab0d6ccdaa819506addae7ed48
Jithendra <jithendrads@gmail.com>
2021-01-16 19:13:13 +0530

    created zuul api gateway

---
+++ PhotoAppApiSpringCloudApiGateway/src/test/java/com/jithendra/PhotoAppApiSpringCloudApiGateway/PhotoAppApiSpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jithendra.PhotoAppApiSpringCloudApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiSpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppApiZuulGateway/src/test/java/com/jithendra/PhotoAppApiZuulGateway/PhotoAppApiZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jithendra.PhotoAppApiZuulGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
